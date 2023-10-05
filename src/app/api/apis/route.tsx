558cd5bc69794885d851a9135f1cd3353f4cc090
Jonathan Scion <yonision@hotmail.com>
2023-10-05 06:20:17 -0600

    basic table css

--- src/app/api/apis/route.tsx
+++ src/app/api/apis/route.tsx
@@ -3,11 +3,12 @@ import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request) {
-    const apis = await prisma.apis.findMany();
+    //const apis = await prisma.apis.findMany();
+    const apis = await prisma.$queryRaw`SELECT * FROM apis`;
 
     console.log(apis);
 
-    return NextResponse.json(apis)
+    return NextResponse.json(apis);
 
 }
 
